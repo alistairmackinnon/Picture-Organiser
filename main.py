bbc4e523eff6b7e442814a1dc15c09f39efc2164
Ali <alistair.mackinnon@outlook.com>
2020-03-02 23:22:24 +0000

    including date extract

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 from PIL import Image, ExifTags
 import os
+from datetime import datetime
+from shutil import copyfile
 
-folder = "/home/ali/Pictures/nights out"
+src_folder = "/home/ali/Pictures/nights out"
+dst_folder = "/home/ali/Pictures"
 
 
-for root, directory, files in os.walk(folder):
+for root, directory, files in os.walk(src_folder):
     for file in files:
         try:
-            file_path = os.path.join(folder, file)
-            img = Image.open(file_path)
+            src_file_path = os.path.join(src_folder, file)
+            img = Image.open(src_file_path)
             exif = {ExifTags.TAGS[k]: v for k, v in img._getexif().items() if k in ExifTags.TAGS}
-            print(file_path)
-            print(exif['DateTimeOriginal'])
+            crt_timestamp = exif['DateTimeOriginal']
+            print(src_file_path)
+            date_str = crt_timestamp[:10].replace(':', '-')
+            date_obj = datetime.strptime(date_str, "%Y-%m-%d")
+            print(date_obj.year)
+
         except:
             continue
