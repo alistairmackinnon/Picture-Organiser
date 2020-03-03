73a99a4b5d3654ec0c161161d26098d950492df0
Ali <alistair.mackinnon@outlook.com>
2020-03-03 19:11:30 +0000

    working creating nexted folder

--- main.py
+++ main.py
@@ -1,23 +1,43 @@
 from PIL import Image, ExifTags
+import PIL
 import os
 from datetime import datetime
 from shutil import copyfile
 
-src_folder = "/home/ali/Pictures/nights out"
-dst_folder = "/home/ali/Pictures"
 
+def get_year(file_in):
+    exif = {ExifTags.TAGS[k]: v for k, v in file_in._getexif().items() if k in ExifTags.TAGS}
+    crt_timestamp = exif['DateTimeOriginal']
+    print(src_file_path)
+    date_str = crt_timestamp[:10].replace(':', '-')
+    date_obj = datetime.strptime(date_str, "%Y-%m-%d")
+    year_str = date_obj.year
+    return year_str
+
+
+src_folder = "/home/ali/Pictures/Loch Lomond"
+dst_parent = "/home/ali/Pictures/Yearly_Archive"
+error_count = 0
+error_list = []
+
+if not os.path.exists(dst_parent):
+    os.mkdir(dst_parent)
 
 for root, directory, files in os.walk(src_folder):
     for file in files:
         try:
             src_file_path = os.path.join(src_folder, file)
             img = Image.open(src_file_path)
-            exif = {ExifTags.TAGS[k]: v for k, v in img._getexif().items() if k in ExifTags.TAGS}
-            crt_timestamp = exif['DateTimeOriginal']
-            print(src_file_path)
-            date_str = crt_timestamp[:10].replace(':', '-')
-            date_obj = datetime.strptime(date_str, "%Y-%m-%d")
-            print(date_obj.year)
+            year = get_year(img)
+            dst_dir = os.path.join(dst_parent, str(year))
+            if os.path.isdir(dst_dir):
+                copyfile(src_file_path, os.path.join(dst_dir, file))
+            else:
+                os.mkdir(dst_dir)
 
-        except:
-            continue
+        except PIL.UnidentifiedImageError:
+            error_count += 1
+            error_list.append(src_file_path)
+            pass
+
+print(error_list)
